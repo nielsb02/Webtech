946c57f465db42d1d05372d7af6c833d1fc2efe0
gp2001 <g.shamon@students.uu.nl>
2021-03-16 13:36:36 +0100

    js

--- Webtech/main.js
+++ Webtech/main.js
@@ -1,14 +1,50 @@
 var article = document.getElementsByTagName("ARTICLE")[0];
 var section = document.createElement("SECTION");
 article.appendChild(section);
-
 var p = document.createElement("p");
 var strong = document.createElement("STRONG");
-var em = document.createElement("EM");
-var d = document.createTextNode("Voice Recognition");
-em.appendChild(d);
 p.appendChild(strong);
-var c = document.createTextNode("Where does the feature" + d + "belong to?");
-strong.appendChild(c);
 section.appendChild(p);
 section.setAttribute("class", "question");
+var a = ["Where does the feature 'Voice Recognition' belong to?",
+ "Where does the feature 'Colors with Good Contrast' belong to?",
+ "To which main reason does this sentence belong to?",
+ "What element should replace the dots?",
+ "What element should replace the dots?"];
+
+class question{
+    constructor(a)
+    {
+        this.title = a;
+    }
+}
+question1 = new question(a[0]);
+question2 = new question(a[1]);
+question3 = new question(a[2]);
+question4 = new question(a[3]);
+question5 = new question(a[4]);
+questions = [{question1},{question2},{question3},{question4},{question5}];
+
+function whichquestion()
+{
+
+
+    titel(question1.title);
+
+    titel(question2.title);
+
+    titel(question3.title);
+
+    titel(question4.title);
+
+    titel(question5.title);
+
+}
+
+function titel(titel1)
+{
+    var x = document.createTextNode(titel1);
+    strong.appendChild(x);
+}
+class multiplechoice{}
+class fillin{}
