36b562e012f20cd1da96b5249d888e3a50be5b3f
nielsb02 <72166173+nielsb02@users.noreply.github.com>
2021-03-16 17:14:29 +0100

    multiple choiche

--- Webtech/main.js
+++ Webtech/main.js
@@ -1,11 +1,10 @@
 var article = document.getElementsByTagName("ARTICLE")[0];
-var section = document.createElement("SECTION");
-article.appendChild(section);
-var p = document.createElement("p");
-var strong = document.createElement("STRONG");
-p.appendChild(strong);
-section.appendChild(p);
-section.setAttribute("class", "question");
+var section;
+
+choiceSpan = [];
+
+var strong;  
+
 var n = 0;
 const titlearray = ["Where does the feature 'Voice Recognition' belong to?",
  "Where does the feature 'Colors with Good Contrast' belong to?",
@@ -14,8 +13,11 @@ const titlearray = ["Where does the feature 'Voice Recognition' belong to?",
  "What element should replace the dots?"];
 const answerarray = ["Physical disability", "Visual Disability","Commercial reasons","ul", "aside"];
 const optionsarray = [["Visual Disability","Hearing Disability","Cognitive Disability","Physical Disability"],
-["Visual Disability","Hearing Disability","Cognitive Disability","Physical Disability"],
+["Visual Disability","Hearing Disability","Cognitive Disability","a"],
 ["Ethical reasons","Reputational reasons","Legal reasons","Commercial reasons"]];
+
+
+
 class question{
     constructor(qtitle, qanswer)
     {
@@ -31,81 +33,71 @@ class question{
 </label>*/
 
 class multiplechoice extends question{
-    constructor(title, answer, options){
-    super(title, answer);
-    this.options = options;
-    this.label = document.createElement("LABEL");
-    this.input = document.createElement("INPUT");
-    this.spanCss = document.createElement("SPAN");
-    this.spanText = document.createElement("SPAN");
-    let e = document.createTextNode(options[0]);
-    let f = document.createTextNode(options[1]);
-    let g = document.createTextNode(options[2]);
-    let h = document.createTextNode(options[3]);
-    this.input.setAttribute("type", "radio");
-    this.input.setAttribute("name", "qoptions");
-    this.spanCss.setAttribute("class", "design");
-    d.appendChild(e);
-    a.appendChild(b);
-    a.appendChild(c);
-    a.appendChild(d);
-    section.appendChild(a);
-    let x = a.cloneNode(true);
-    let y = x.getElementsByTagName("SPAN")[1];
-    let z = y.childNodes[0];
-    y.replaceChild(f, z);
-    section.appendChild(x);
-    let o = a.cloneNode(true);
-    let p = o.getElementsByTagName("SPAN")[1];
-    let l = p.childNodes[0];
-    p.replaceChild(g, l);
-    section.appendChild(o);
-    let om = a.cloneNode(true);
-    let pm = om.getElementsByTagName("SPAN")[1];
-    let lm = pm.childNodes[0];
-    pm.replaceChild(h, lm);
-    section.appendChild(om);
-
-  /*  d.replaceChild(g);
-    section.appendChild(a);
-    d.replaceChild(h);
-    section.appendChild(a);*/
+    constructor(qtitle, answer, optionss){
+    super(qtitle, answer);
+    this.options = optionss;
     }
-
 }
 
-multiplechoice.prototype.CreateOptions = function()
+multiplechoice.prototype.createOptions = function()
 {
+    var x = document.createTextNode(this.title);
+    strong.appendChild(x);
 
+    for(var i = 0; i < this.options.length; i++)
+    {
+        var label = document.createElement("LABEL");
+        var input = document.createElement("INPUT");
+        var spanCss = document.createElement("SPAN");
+        choiceSpan[i] = document.createElement("SPAN");
+        input.setAttribute("type", "radio");
+        input.setAttribute("name", "qoptions");
+        spanCss.setAttribute("class", "design");
+
+        choiceSpan[i].appendChild(document.createTextNode(this.options[i]));
+
+        label.appendChild(input);
+        label.appendChild(spanCss);
+        label.appendChild(choiceSpan[i]);
+
+        section.appendChild(label);
+    }
+}
+
+multiplechoice.prototype.changeOptions = function()
+{
+    var titleNode = document.createTextNode(this.title);
+    strong.replaceChild(titleNode, strong.firstChild)
+    for(var i = 0; i < this.options.length; i++)
+    {
+        var text = document.createTextNode(this.options[i]);
+        choiceSpan[i].replaceChild(text, choiceSpan[i].firstChild);
+    }
 }
 
 
 class fillin extends question{
     constructor(a){
-        
         super()}
-
 }
-question1 = new multiplechoice(titlearray[0], answerarray[0], optionsarray[0]);
-//question2 = new multiplechoice(titlearray[1], answerarray[1], optionsarray[1]);
-//question3 = new multiplechoice(titlearray[2], answerarray[2], optionsarray[2]);
-/*question4 = new fillin(a[3], z[3]);
-question5 = new fillin(a[4], z[4]);
-questions = [{question1},{question2},{question3},{question4},{question5}];*/
 
-titel(question2.title);
 
-function whichquestion()
+
+function layout()
 {
+    section = document.createElement("SECTION");
+    article.appendChild(section);
+    section.setAttribute("class", "question");
+
+    var p = document.createElement("p");
+    section.appendChild(p);
+
+    strong = document.createElement("STRONG");
+    p.appendChild(strong);
     
-    titel(questions[n].title);
 
-}
-
-function titel(titel1)
-{
-    var x = document.createTextNode(titel1);
-    strong.appendChild(x);
+    question1 = new multiplechoice(titlearray[0], answerarray[0], optionsarray[0]);
+    question1.createOptions();
 
     let a = document.createElement("INPUT");
     a.setAttribute("type", "button");
@@ -125,8 +117,23 @@ function titel(titel1)
     section.appendChild(a);
     section.appendChild(b);
     section.appendChild(c);
-
 }
+
+layout();
+
+
+question2 = new multiplechoice(titlearray[1], answerarray[1], optionsarray[1]);
+//question2.changeOptions();
+question3 = new multiplechoice(titlearray[2], answerarray[2], optionsarray[2]);
+question3.changeOptions();
+/*question4 = new fillin(a[3], z[3]);
+question5 = new fillin(a[4], z[4]);
+questions = [{question1},{question2},{question3},{question4},{question5}];*/
+function whichquestion()
+{
+    titel(questions[n].title);
+}
+
 
 
 
