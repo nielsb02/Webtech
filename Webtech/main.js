2c1340b0a206e96a1428f26e2c4f4c66470db071
gp2001 <g.shamon@students.uu.nl>
2021-03-16 16:04:18 +0100

    q

--- Webtech/main.js
+++ Webtech/main.js
@@ -6,38 +6,93 @@ var strong = document.createElement("STRONG");
 p.appendChild(strong);
 section.appendChild(p);
 section.setAttribute("class", "question");
-var a = ["Where does the feature 'Voice Recognition' belong to?",
+var n = 0;
+const titlearray = ["Where does the feature 'Voice Recognition' belong to?",
  "Where does the feature 'Colors with Good Contrast' belong to?",
  "To which main reason does this sentence belong to?",
  "What element should replace the dots?",
  "What element should replace the dots?"];
-
+const answerarray = ["Physical disability", "Visual Disability","Commercial reasons","ul", "aside"];
+const optionsarray = [["Visual Disability","Hearing Disability","Cognitive Disability","Physical Disability"],
+["Visual Disability","Hearing Disability","Cognitive Disability","Physical Disability"],
+["Ethical reasons","Reputational reasons","Legal reasons","Commercial reasons"]];
 class question{
-    constructor(a)
+    constructor(qtitle, qanswer)
     {
-        this.title = a;
+        this.title = qtitle;
+        this.answer = qanswer;
     }
 }
-question1 = new question(a[0]);
-question2 = new question(a[1]);
-question3 = new question(a[2]);
-question4 = new question(a[3]);
-question5 = new question(a[4]);
-questions = [{question1},{question2},{question3},{question4},{question5}];
+
+/*<label>
+<input type="radio" name="question1">
+<span class="design"></span>
+<span>Visual Disability</span>
+</label>*/
+
+class multiplechoice extends question{
+    constructor(title, answer, options){
+    super(title, answer);
+    let a = document.createElement("LABEL");
+    let b = document.createElement("INPUT");
+    let c = document.createElement("SPAN");
+    let d = document.createElement("SPAN");
+    let e = document.createTextNode(options[0]);
+    let f = document.createTextNode(options[1]);
+    let g = document.createTextNode(options[2]);
+    let h = document.createTextNode(options[3]);
+    b.setAttribute("type", "radio");
+    b.setAttribute("name", "question1");
+    c.setAttribute("class", "design");
+    d.appendChild(e);
+    a.appendChild(b);
+    a.appendChild(c);
+    a.appendChild(d);
+    section.appendChild(a);
+    let x = a.cloneNode(true);
+    let y = x.getElementsByTagName("SPAN")[1];
+    let z = y.childNodes[0];
+    y.replaceChild(f, z);
+    section.appendChild(x);
+    let o = a.cloneNode(true);
+    let p = o.getElementsByTagName("SPAN")[1];
+    let l = p.childNodes[0];
+    p.replaceChild(g, l);
+    section.appendChild(o);
+    let om = a.cloneNode(true);
+    let pm = om.getElementsByTagName("SPAN")[1];
+    let lm = pm.childNodes[0];
+    pm.replaceChild(h, lm);
+    section.appendChild(om);
+
+  /*  d.replaceChild(g);
+    section.appendChild(a);
+    d.replaceChild(h);
+    section.appendChild(a);*/
+    }
+
+}
+
+
+class fillin extends question{
+    constructor(a){
+        
+        super()}
+
+}
+question1 = new multiplechoice(titlearray[0], answerarray[0], optionsarray[0]);
+//question2 = new multiplechoice(titlearray[1], answerarray[1], optionsarray[1]);
+//question3 = new multiplechoice(titlearray[2], answerarray[2], optionsarray[2]);
+/*question4 = new fillin(a[3], z[3]);
+question5 = new fillin(a[4], z[4]);
+questions = [{question1},{question2},{question3},{question4},{question5}];*/
+
+titel(question2.title);
 
 function whichquestion()
 {
-
-
-    titel(question1.title);
-
-    titel(question2.title);
-
-    titel(question3.title);
-
-    titel(question4.title);
-
-    titel(question5.title);
+    
+    titel(questions[n].title);
 
 }
 
@@ -45,6 +100,27 @@ function titel(titel1)
 {
     var x = document.createTextNode(titel1);
     strong.appendChild(x);
+
+    let a = document.createElement("INPUT");
+    a.setAttribute("type", "button");
+    a.setAttribute("value", "check");
+    a.setAttribute("class", "qbutton1");
+
+    let b = document.createElement("INPUT");
+    b.setAttribute("type", "button");
+    b.setAttribute("value", "previous");
+    b.setAttribute("class", "qbutton2");
+
+    let c = document.createElement("INPUT");
+    c.setAttribute("type", "button");
+    c.setAttribute("value", "next");
+    c.setAttribute("class", "qbutton3");
+
+    section.appendChild(a);
+    section.appendChild(b);
+    section.appendChild(c);
+
 }
-class multiplechoice{}
-class fillin{}
+
+
+
